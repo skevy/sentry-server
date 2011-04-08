d6b7f4cdb5964c7a524ac60b0d64b8af8cc2eb06
Adam Miskiewicz <adam.skevy@mac.com>
2011-04-08 18:48:26 -0400

    ….

--- sentry_server/settings.py
+++ sentry_server/settings.py
@@ -1,7 +1,7 @@
 # Django settings for sentry_server project.
 
 import os
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
